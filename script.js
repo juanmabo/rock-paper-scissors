231a094b22cce629e6b873753befa9e3e12ff604
juanmabo <juanmanuelsf2211@gmail.com>
2024-01-20 00:13:39 -0300

    Creada la primer etapa, ya funciona el juego. Falta agregar para que no haya que actualizar la pagina

---
+++ script.js
@@ -0,0 +1,49 @@
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function convertirSeleccion(selection) {
+    selection = selection.toLowerCase();
+
+    switch (selection) {
+        case 'rock':
+            return 1;
+        case 'paper':
+            return 2;
+        case 'scissors':
+            return 3;
+        default:
+            return null;
+    }
+}
+
+let computerSelection = getComputerChoice();
+
+let input = prompt("Put your choice (Rock, Paper or Scissors):");
+
+let playerSelection = convertirSeleccion(input);
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's tie. The computer choose the same.";
+    } else if (playerSelection === 1 && computerSelection === 2) {
+        return "You lost. Computer choose Paper"
+    } else if (playerSelection === 1 && computerSelection === 3) {
+        return "You won. Computer choose Scissors"
+    } else if (playerSelection === 2 && computerSelection === 1) {
+        return "You won. Computer choose Rock"
+    } else if (playerSelection === 2 && computerSelection === 3) {
+        return "You lost. Computer choose Scissors"
+    } else if (playerSelection === 3 && computerSelection === 1) {
+        return "You lost. Computer choose Rock"
+    } else if (playerSelection === 3 && computerSelection === 2) {
+        return "You won. Computer choose Paper"
+    } else {
+        return "Wrong prompt. Try again"
+    }
+}
+
+
+console.log(playRound());
