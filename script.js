27e081293be7f194bbdc3e1209d9e98289c8a506
juanmabo <juanmanuelsf2211@gmail.com>
2024-01-23 12:54:16 -0300

    reversion del codigo con botones, eventos DOM en una nueva branch utilizando ejemplo de comunidad

--- script.js
+++ script.js
@@ -1,47 +1,118 @@
+const choiceArray = ["Rock", "Paper", "Scissors"];
+
+let playerScore = 0;
+let computerScore = 0;
+
+const rockBtn = document.querySelector("#rockButton");
+const paperBtn = document.querySelector("#paperButton");
+const scissorsBtn = document.querySelector("#scissorsButton");
+
+const player = document.querySelector(".playerScore");
+const computer = document.querySelector(".computerScore");
+
+const roundResult = document.querySelector(".result");
+const playerText = document.createElement("h3");
+const computerText = document.createElement("h3");
+const roundResultText = document.createElement("h3");
+
+const gameWinner = document.createElement("h3");
+const playAgain = document.createElement("button");
+
+rockBtn.addEventListener("click", () => handleClick("Rock"));
+paperBtn.addEventListener("click", () => handleClick("Paper"));
+scissorsBtn.addEventListener("click", () => handleClick("Scissors"));
+
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
+  let randomNumber = Math.floor(Math.random() * 3);
+  return choiceArray[randomNumber];
 }
 
-function convertirSeleccion(selection) {
-    selection = selection.toLowerCase();
-
-    switch (selection) {
-        case 'rock':
-            return 1;
-        case 'paper':
-            return 2;
-        case 'scissors':
-            return 3;
-        default:
-            return null;
-    }
-}
-
-
-
-
-function playRound() {
-    let playerSelection = convertirSeleccion(prompt("Put your choice (Rock, Paper or Scissors):"));
+function handleClick(playerSelection) {
+  if (
+    (playerScore < 5 && computerScore < 5) &&
+    !(playerScore == 5 && computerScore == 5)
+  ) {
     let computerSelection = getComputerChoice();
-    if (playerSelection == computerSelection) {
-        return "It's a tie. The computer choose the same.";
-    } else if (playerSelection === 1 && computerSelection === 2) {
-        return "You lost. Computer choose Paper"
-    } else if (playerSelection === 1 && computerSelection === 3) {
-        return "You won. Computer choose Scissors"
-    } else if (playerSelection === 2 && computerSelection === 1) {
-        return "You won. Computer choose Rock"
-    } else if (playerSelection === 2 && computerSelection === 3) {
-        return "You lost. Computer choose Scissors"
-    } else if (playerSelection === 3 && computerSelection === 1) {
-        return "You lost. Computer choose Rock"
-    } else if (playerSelection === 3 && computerSelection === 2) {
-        return "You won. Computer choose Paper"
-    } else {
-        return "Wrong prompt. Try again"
-    }
+    playRound(playerSelection, computerSelection);
+    showRoundResult();
+    updateScore();
+    game();
+  }
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerText.textContent = `You choose ${playerSelection}`;
+  computerText.textContent = `Computer choose ${computerSelection}`;
 
+  if (playerSelection === computerSelection) {
+    roundResultText.textContent = `It's a Tie! ${playerSelection} ties with ${computerSelection}`;
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    computerScore += 1;
+    roundResultText.textContent = `You Lost! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    playerScore += 1;
+    roundResultText.textContent = `You Won! ${playerSelection} beats ${computerSelection}`;
+  }
+}
 
-console.log(playRound());
+function updateScore() {
+  player.textContent = playerScore;
+  computer.textContent = computerScore;
+}
+
+function showRoundResult() {
+  playerText.classList.add("roundResultText");
+  computerText.classList.add("roundResultText");
+  roundResultText.classList.add("roundResultText");
+  roundResult.append(playerText, computerText, roundResultText);
+}
+
+function removeRoundResult() {
+  while (roundResult.firstChild) {
+    roundResult.removeChild(roundResult.firstChild);
+  }
+}
+
+function game() {
+  if (playerScore === 5 || computerScore === 5) {
+    showGameWinner();
+  }
+}
+
+function gameWinnerResult() {
+  if (playerScore > computerScore) {
+    gameWinner.textContent = "You Won";
+  } else if (computerScore > playerScore) {
+    gameWinner.textContent = "You Lost";
+  } else {
+    gameWinner.textContent = `It's a Tie`;
+  }
+}
+
+function showGameWinner() {
+  removeRoundResult();
+  gameWinnerResult();
+  gameWinner.classList.add("gameWinner");
+  playAgain.textContent = `PLAY AGAIN`;
+  playAgain.classList.add("playAgain");
+  roundResult.append(gameWinner, playAgain);
+}
+
+function removeGameWinner(){
+  while (roundResult.firstChild) {
+    roundResult.removeChild(roundResult.firstChild);
+  }
+}
+
+function restart() {
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
+  removeGameWinner();
+}
+
+playAgain.addEventListener("click", restart);
